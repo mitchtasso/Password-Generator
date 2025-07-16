c4a7c800371d007666ddb195fcd56973aa43987d
mitchtasso <145378736+mitchtasso@users.noreply.github.com>
2025-07-15 23:07:02 -0400

    Upgraded to multilength generator
    Upgraded to multilength generator with adjustable length (8-24)

--- passgen.py
+++ passgen.py
@@ -1,15 +1,16 @@
 import random
 import string
 
-length = 8
 lowercase = string.ascii_lowercase
 uppercase = string.ascii_uppercase
 digits = string.digits
 characters = string.punctuation
+all = lowercase + uppercase + digits + characters
         
-def generatePass():
+def generatePass(length):
     passwordStore = []
     password = ''
+    #Randomly selects and adds characters to the password array, makes sure that at least two of each type exist.
     for i in range(0,length):
         if i <= 1:
             gen = random.randint(0,len(lowercase)-1)
@@ -20,14 +21,28 @@ def generatePass():
         elif i > 3 and i <= 5:
             gen = random.randint(0,len(digits)-1)
             passwordStore.append(digits[gen])
-        elif i > 5 and i <= length:
+        elif i > 5 and i <= 8:
             gen = random.randint(0,len(characters)-1)
             passwordStore.append(characters[gen])
+        else:
+            gen = random.randint(0,len(all)-1)
+            passwordStore.append(all[gen])
     
+    #Shuffles and 
     random.shuffle(passwordStore)
     for x in range(0,length):
         password += passwordStore[x]
     print(password)
 
 if __name__ == "__main__":
-    generatePass()
+    
+    while(True):
+        print("Please enter the desired length for your password. (8-24 Characters)\n")
+        length = input("Length: ")
+        if int(length) >= 8 and int(length) <= 24:
+            break
+        else:
+            print("\nPlease enter a valid length.\n")
+            continue
+    
+    generatePass(int(length))
